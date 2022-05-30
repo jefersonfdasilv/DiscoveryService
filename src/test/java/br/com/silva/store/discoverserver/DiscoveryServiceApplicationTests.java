b46f4043ccfc14ebb0a9a352f4433f942e2857d9
JEFERSON FERREIRA DA SILVA <jeferson.ferreira.ca@gmail.com>
2022-05-30 17:25:48 -0300

    first commit

---
+++ src/test/java/br/com/silva/store/discoverserver/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.silva.store.discoverserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
